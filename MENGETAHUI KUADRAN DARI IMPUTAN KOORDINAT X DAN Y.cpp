47355710ed272041883a285ca47d856173f0c1d4
muhammadyusufalfaqih <45525489+muhammadyusufalfaqih@users.noreply.github.com>
2018-12-30 15:50:20 -0800

    Add files via upload

---
+++ MENGETAHUI KUADRAN DARI IMPUTAN KOORDINAT X DAN Y.cpp
@@ -0,0 +1,22 @@
+#include<stdio.h>
+using namespace std;
+void pecahan ();
+main ()
+{
+    pecahan ();
+}
+void pecahan ()
+{
+    int a,b,l,t;
+    printf("== Menyederhanakan Bilangan Pecahan ==\n\n");
+    printf(" masukan Pembilang : ");
+    scanf ("%d", &a);
+    printf(" masukan Penyebut : ");
+    scanf ("%d", &b);
+    t=a/b;
+    l=a%b;
+    if (a%b==0)
+        printf ("Bilangan (%d/%d)\n disederhanakan Menjadi %d",a,b,t);
+    else if (a%b !=0)
+        printf (" Bilangan (%d/d%)\n Disederhanakan Menjadi (%d/%d)",a,b,t,l,b);
+}
